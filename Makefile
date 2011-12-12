ce63b5883454af07f5f976a3c2dbb6d2594577a9
Andrea Sansottera <andrea.sansottera@gmail.com>
2011-12-12 08:34:50 +0000

    Added MinMaxHeap implementation and unit tests. Added makefile. Set ignore properties on dep and obj folders.

---
+++ Makefile
@@ -0,0 +1,62 @@
+OBJS=ut.o utMinMaxHeap.o
+OBJ_OPT_FILES=$(patsubst %.o,obj/opt/%.o,$(OBJS))
+OBJ_DBG_FILES=$(patsubst %.o,obj/dbg/%.o,$(OBJS))
+
+DEP_FILES=$(patsubst %.o,dep/%.d,$(OBJS))
+
+INCLUDE_DIRS=-Iinclude -I/opt/boost
+LIBS=-L/opt/boost/stage/lib -lboost_unit_test_framework -lrt
+DEFINE=-DBOOST_TEST_DYN_LINK
+
+ifeq ($(TOOLS),intel)
+CXX=icpc
+DEP=icpc
+LINK=icpc
+DEPFLAGS=-std=c++0x
+CXXFLAGS_DBG=-O0 -g -std=c++0x -Wall -Wsign-compare -Wcheck -wd279 $(INCLUDE_DIRS)
+CXXFLAGS_OPT=-O2 -xHost -std=c++0x -Wall -Wsign-compare -Wcheck -wd279 $(INCLUDE_DIRS)
+CXXFLAGS_TGT=-O2 -march=core2 -std=c++0x -Wall -Wsign-compare -Wcheck -wd279 $(INCLUDE_DIRS)
+CXXFLAGS_PROF=-O2 -g -xHost -std=c++0x -Wall -Wsign-compare -Wcheck -wd279 $(INCLUDE_DIRS)
+else
+CXX=g++
+DEP=g++
+LINK=g++
+DEPFLAGS=-std=c++0x
+CXXFLAGS_DBG=-O0 -g -std=c++0x -Wall -Wsign-compare $(INCLUDE_DIRS)
+CXXFLAGS_OPT=-O2 -march=native -std=c++0x -Wall -Wsign-compare $(INCLUDE_DIRS)
+CXXFLAGS_TGT=-O2 -march=core2 -std=c++0x -Wall -Wsign-compare $(INCLUDE_DIRS)
+CXXFLAGS_PROF=-O2 -g -march=native -std=c++0x -Wall -Wsign-compare $(INCLUDE_DIRS)
+endif
+
+all: bin/sway_test_opt bin/sway_test_dbg
+
+opt: bin/sway_test_opt
+
+dbg: bin/sway_test_dbg
+
+bin/sway_test_opt: $(OBJ_OPT_FILES)
+	$(LINK) $(OBJ_OPT_FILES) $(LIBS) -o $@
+
+bin/sway_test_dbg: $(OBJ_DBG_FILES)
+	$(LINK) $(OBJ_DBG_FILES) $(LIBS) -o $@
+
+ifneq ($(MAKECMDGOALS),clean)
+-include $(DEP_FILES)
+endif
+
+dep/%.d: src/%.cpp include/*.h
+	$(DEP) $(DEPFLAGS) -MM $(patsubst dep/%.d,src/%.cpp,$@) -MT $(patsubst dep/%.d,obj/opt/%.o,$@) -MT $(patsubst dep/%.d,obj/dbg/%.o,$@) $(INCLUDE_DIRS) > $@
+
+obj/dbg/%.o: src/%.cpp
+	$(CXX) $(DEFINE) -c $(CXXFLAGS_DBG) $< -o $@
+
+obj/opt/%.o: src/%.cpp
+	$(CXX) $(DEFINE) -c $(CXXFLAGS_OPT) $< -o $@
+
+clean:
+	rm -f bin/*
+	rm -f dep/*.d
+	rm -f obj/opt/*.o
+	rm -f obj/dbg/*.o
+
+.PHONY: clean
---
+++ src/utMinMaxHeap.cpp
@@ -0,0 +1,278 @@
+#include <boost/test/unit_test.hpp>
+
+#include "MinMaxHeap.h"
+#include <vector>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+using namespace sway;
+
+void CheckMinMaxHeapPropertyPtr(vector<int *> & v) {
+	for (vector<int *>::iterator i = v.begin(); i < v.end(); i++) {
+		bool odd = getLevel(v.begin(), v.end(), i) % 2 == 1;
+		stack<vector<int *>::iterator> descendants;
+		descendants.push(getLeftChild(v.begin(), v.end(), i));
+		descendants.push(getRightChild(v.begin(), v.end(), i));
+		while (descendants.size() > 0) {
+			vector<int *>::iterator j = descendants.top();
+			descendants.pop();
+			if (j != v.end()) {
+				if (odd)
+					BOOST_REQUIRE_LE(**j, **i);
+				else
+					BOOST_REQUIRE_GE(**j, **i);
+				descendants.push(getLeftChild(v.begin(), v.end(), j));
+				descendants.push(getRightChild(v.begin(), v.end(), j));
+			}
+		}
+	}
+}
+
+void CheckMinMaxHeapProperty(vector<int> & v) {
+	for (vector<int>::iterator i = v.begin(); i < v.end(); i++) {
+		bool odd = getLevel(v.begin(), v.end(), i) % 2 == 1;
+		stack<vector<int>::iterator> descendants;
+		descendants.push(getLeftChild(v.begin(), v.end(), i));
+		descendants.push(getRightChild(v.begin(), v.end(), i));
+		while (descendants.size() > 0) {
+			vector<int>::iterator j = descendants.top();
+			descendants.pop();
+			if (j != v.end()) {
+				if (odd)
+					BOOST_REQUIRE_LE(*j, *i);
+				else
+					BOOST_REQUIRE_GE(*j, *i);
+				descendants.push(getLeftChild(v.begin(), v.end(), j));
+				descendants.push(getRightChild(v.begin(), v.end(), j));
+			}
+		}
+	}
+}
+
+void print(vector<int> & v) {
+	for (vector<int>::iterator i = v.begin(); i != v.end(); i++) {
+		std::cout << *i << ((i != v.end() - 1) ? ", " : "");
+	}
+	std::cout << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(TestMakeHeap) {
+
+	vector<int> v;
+
+	for (int i = 0; i <= 100; i++) {
+		v.push_back(i);
+	}
+
+	make_minmaxheap(v.begin(), v.end());
+
+	CheckMinMaxHeapProperty(v);
+}
+
+template<class T>
+struct IndirectComp {
+public:
+	bool operator()(T * a, T * b) {
+		return *a < *b;
+	}
+};
+
+BOOST_AUTO_TEST_CASE(TestMakeHeapComp) {
+
+	int a[101];
+	vector<int *> v;
+	for (int i = 0; i <= 100; i++) {
+		a[i] = 101 - i;
+		v.push_back(a + i);
+	}
+
+	make_minmaxheap(v.begin(), v.end(), IndirectComp<int>());
+
+	CheckMinMaxHeapPropertyPtr(v);
+}
+
+BOOST_AUTO_TEST_CASE(TestPopMinHeap) {
+
+	vector<int> v;
+	
+	for (int i = 0; i <= 100; i++) {
+		v.push_back(i);
+	}
+
+	make_minmaxheap(v.begin(), v.end());
+
+	popmin_minmaxheap(v.begin(), v.end());
+	v.pop_back();
+
+	vector<int>::iterator found = find(v.begin(), v.end(), 0);
+	BOOST_REQUIRE(found == v.end());
+	BOOST_REQUIRE(v.size() == 100);
+
+	CheckMinMaxHeapProperty(v);
+}
+
+BOOST_AUTO_TEST_CASE(TestPopMinHeapComp) {
+
+	int a[101];
+	vector<int *> v;
+	for (int i = 0; i <= 100; i++) {
+		a[i] = 101 - i;
+		v.push_back(a + i);
+	}
+
+	make_minmaxheap(v.begin(), v.end(), IndirectComp<int>());
+
+	popmin_minmaxheap(v.begin(), v.end(), IndirectComp<int>());
+	v.pop_back();
+
+	vector<int *>::iterator found = find(v.begin(), v.end(), a + 100);
+	BOOST_REQUIRE(found == v.end());
+	BOOST_REQUIRE(v.size() == 100);
+
+	CheckMinMaxHeapPropertyPtr(v);
+}
+
+BOOST_AUTO_TEST_CASE(TestPopMaxHeap) {
+
+	vector<int> v;
+	
+	for (int i = 0; i <= 100; i++) {
+		v.push_back(i);
+	}
+
+	make_minmaxheap(v.begin(), v.end());
+
+	popmax_minmaxheap(v.begin(), v.end());
+	v.pop_back();
+
+	vector<int>::iterator found = find(v.begin(), v.end(), 100);
+	BOOST_REQUIRE(found == v.end());
+
+	BOOST_REQUIRE(v.size() == 100);
+
+	CheckMinMaxHeapProperty(v);
+}
+
+BOOST_AUTO_TEST_CASE(TestPopMaxHeapComp) {
+
+	int a[101];
+	vector<int *> v;
+	for (int i = 0; i <= 100; i++) {
+        a[i] = 101 - i;
+        v.push_back(a + i);
+    }
+
+	make_minmaxheap(v.begin(), v.end(), IndirectComp<int>());
+
+	popmax_minmaxheap(v.begin(), v.end(), IndirectComp<int>());
+	v.pop_back();
+
+	vector<int *>::iterator found = find(v.begin(), v.end(), a + 0);
+	BOOST_REQUIRE(found == v.end());
+
+	BOOST_REQUIRE(v.size() == 100);
+
+	CheckMinMaxHeapPropertyPtr(v);
+}
+
+BOOST_AUTO_TEST_CASE(TestPushHeap) {
+
+	vector<int> v;
+
+	for (int i = 0; i <= 100; i++) {
+		v.push_back(i);
+	}
+
+	make_minmaxheap(v.begin(), v.end());
+
+	v.push_back(200);
+	push_minmaxheap(v.begin(), v.end());
+
+	BOOST_REQUIRE(v.size() == 102);
+	BOOST_CHECK(find(v.begin(), v.end(), 200) == v.begin() + 1 || find(v.begin(), v.end(), 200) == v.begin() + 2);
+
+	CheckMinMaxHeapProperty(v);
+
+}
+
+BOOST_AUTO_TEST_CASE(TestPushHeapComp) {
+
+	int a[101];
+	vector<int *> v;
+	for (int i = 0; i <= 100; i++) {
+        a[i] = 101 - i;
+        v.push_back(a + i);
+    }
+	int b = 200;
+
+	make_minmaxheap(v.begin(), v.end(), IndirectComp<int>());
+
+	v.push_back(&b);
+	push_minmaxheap(v.begin(), v.end(), IndirectComp<int>());
+
+	BOOST_REQUIRE(v.size() == 102);
+	BOOST_CHECK(find(v.begin(), v.end(), &b) == v.begin() + 1 || find(v.begin(), v.end(), &b) == v.begin() + 2);
+
+	CheckMinMaxHeapPropertyPtr(v);
+
+}
+
+BOOST_AUTO_TEST_CASE(TestMakeSmall2) {
+
+	vector<int> a;
+	a.push_back(10);
+	a.push_back(5);
+
+	make_minmaxheap(a.begin(), a.end());
+
+	BOOST_CHECK_EQUAL(a[0], 5);
+	BOOST_CHECK_EQUAL(a[1], 10);
+}
+
+BOOST_AUTO_TEST_CASE(TestPushSmall12) {
+
+	vector<int> a;
+	a.push_back(10);
+
+	make_minmaxheap(a.begin(), a.end());
+
+	a.push_back(5);
+	push_minmaxheap(a.begin(), a.end());
+
+	BOOST_CHECK_EQUAL(a[0], 5);
+	BOOST_CHECK_EQUAL(a[1], 10);
+}
+
+BOOST_AUTO_TEST_CASE(TestPushSmall02) {
+
+	vector<int> a;
+
+	make_minmaxheap(a.begin(), a.end());
+	
+	a.push_back(10);
+	push_minmaxheap(a.begin(), a.end());
+	
+	a.push_back(5);
+	push_minmaxheap(a.begin(), a.end());
+
+	BOOST_CHECK_EQUAL(a[0], 5);
+	BOOST_CHECK_EQUAL(a[1], 10);
+}
+
+BOOST_AUTO_TEST_CASE(TestSmallExceed) {
+
+	vector<int> a;
+	a.push_back(10);
+	a.push_back(5);
+
+	make_minmaxheap(a.begin(), a.end());
+
+	BOOST_CHECK_EQUAL(a[0], 5);
+	BOOST_CHECK_EQUAL(a[1], 10);
+
+	popmax_minmaxheap(a.begin(), a.end());
+	a.pop_back();
+
+	BOOST_CHECK_EQUAL(a[0], 5);
+}
